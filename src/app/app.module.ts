e85648d48e5da2211f9a70c4eee16877c23f7ceb
DanielCollaguazo2003 <109354364+DanielCollaguazo2003@users.noreply.github.com>
2024-02-06 09:55:01 -0500

    subida del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { VistaRecargaComponent } from './pages/vista-recarga/vista-recarga.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './layout/header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VistaRecargaComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
