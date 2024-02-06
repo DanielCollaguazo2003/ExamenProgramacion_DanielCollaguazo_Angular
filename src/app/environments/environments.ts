65138bbaa0169ce1887590715ab65fb1a4befbff
DanielCollaguazo2003 <109354364+DanielCollaguazo2003@users.noreply.github.com>
2024-02-06 10:50:18 -0500

    environment

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
     //Integracion con jakarta
-    WS_PATH: 'http://localhost:8080/demo63/rs'
+    WS_PATH: 'http://localhost:8080/examenRecargas_CollaguazoDaniel_JEE/rs'
 };
